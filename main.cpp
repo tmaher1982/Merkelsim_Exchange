4d450dbbf7ac1e36362a55d0b82030c45091208c
Tamer Maher <tmaher1982@gmail.com>
2021-04-14 17:55:05 +0200

    Basic options

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>?
+
+int main()
+{
+    // 1 print help
+    std::cout << "1: Print help " << std::endl;
+
+    // 2 print exchange stats
+    std::cout << "2: Print exchange stats " << std::endl;
+
+    // 3 make an offer
+    std::cout << "3: Make an offer " << std::endl;
+
+    // 4 make a bid
+    std::cout << "4: Make a bid " << std::endl;
+
+    // 5 print wallet
+    std::cout << "5: Print Walled " << std::endl;
+
+    // 6 continue
+    std::cout << "6: Continue " << std::endl;
+
+    return 0;
+}
