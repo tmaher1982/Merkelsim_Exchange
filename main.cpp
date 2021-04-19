f19e492ddcae0e7cc1fe84a992b57cb90a93e24c
Tamer Maher <tmaher1982@gmail.com>
2021-04-19 09:21:44 +0200

    printMenu refactoring

--- main.cpp
+++ main.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 #include <cstdlib>
 
+void printMenu()
+{
+    // 1 print help
+    std::cout << "1: Print help " << std::endl;
+
+    // 2 print exchange stats
+    std::cout << "2: Print exchange stats " << std::endl;
+
+    // 3 make an offer
+    std::cout << "3: Make an offer " << std::endl;
+
+    // 4 make a bid
+    std::cout << "4: Make a bid " << std::endl;
+
+    // 5 print wallet
+    std::cout << "5: Print Walled " << std::endl;
+
+    // 6 continue
+    std::cout << "6: Continue " << std::endl;
+
+    // 7 exit
+    std::cout << "7: Exit " << std::endl;
+
+    std::cout << "=============== " << std::endl;
+    std::cout << "Type in 1-7 " << std::endl;   
+}
+
 int main()
 {
-    while (true)
+    
+    while(true)
     {
-        // 1 print help
-        std::cout << "1: Print help " << std::endl;
-
-        // 2 print exchange stats
-        std::cout << "2: Print exchange stats " << std::endl;
-
-        // 3 make an offer
-        std::cout << "3: Make an offer " << std::endl;
-
-        // 4 make a bid
-        std::cout << "4: Make a bid " << std::endl;
-
-        // 5 print wallet
-        std::cout << "5: Print Walled " << std::endl;
-
-        // 6 continue
-        std::cout << "6: Continue " << std::endl;
-
-        // 7 exit
-        std::cout << "7: Exit " << std::endl;
-
-        std::cout << "=============== " << std::endl;
-        std::cout << "Type in 1-7 " << std::endl;
-
+        printMenu();
+        
         int userOption;
         std::cin >> userOption;
 
