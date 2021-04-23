aa33a5a9e4227cfb76e9e87be69cf16e49770c65
Tamer Maher <tmaher1982@gmail.com>
2021-04-23 23:40:23 +0200

    basic data presentation with double and string

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 void printMenu()
 {
@@ -130,7 +131,14 @@ void processUserOption (int userOption)
 
 int main()
 {
+    double price = 5319.450228;
+    double amount = 0.00020075;
+
+    std::string timestamp{"2020/03/17 17:01:24.884492"};
+    std::string product{"BTC/USDT"};
+    std::string orderType{"bid"};
     
+
     while(true)
     {
         printMenu();
