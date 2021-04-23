664cad34f975bab0c699b97a57f5b85990d2f1b6
Tamer Maher <tmaher1982@gmail.com>
2021-04-24 00:21:03 +0200

    using vectors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <vector>
 
 void printMenu()
 {
@@ -131,25 +132,44 @@ void processUserOption (int userOption)
 
 int main()
 {
-    double price = 5319.450228;
-    double amount = 0.00020075;
-
-    std::string timestamp{"2020/03/17 17:01:24.884492"};
-    std::string product{"BTC/USDT"};
-    // std::string orderType{"bid"};
-
     enum class OrderBookType{bid, ask};
-    OrderBookType orderType = OrderBookType::bid;
 
+    // double price = 5319.450228;
+    // double amount = 0.00020075;
+    // std::string timestamp{"2020/03/17 17:01:24.884492"};
+    // std::string product{"BTC/USDT"};
+    // OrderBookType orderType = OrderBookType::ask;
+
+    std::vector<double> prices;
+    std::vector<double> amounts;
+    std::vector<std::string> timestamps;
+    std::vector<std::string> products;
+    std::vector<OrderBookType> orderTypes;
+
+    prices.push_back(5000.01);
+    amounts.push_back(0.001);
+    timestamps.push_back("2020/03/17 17:01:24.884492");
+    products.push_back("BTC/USDT");
+    orderTypes.push_back(OrderBookType::bid);
+
+    prices.push_back(10.01);
+    amounts.push_back(0.001);
+    timestamps.push_back("2020/03/17 17:01:24.884492");
+    products.push_back("BTC/USDT");
+    orderTypes.push_back(OrderBookType::ask);
+
+    std::cout << "prices: " << prices[0] << std::endl;
+    std::cout << "prices: " << prices[1] << std::endl;
     
 
 
-    while(true)
-    {
-        printMenu();
-        int userOption = getUserOption();
-        processUserOption(userOption);
-    }
+
+    // while(true)
+    // {
+    //     printMenu();
+    //     int userOption = getUserOption();
+    //     processUserOption(userOption);
+    // }
 
     return 0;
 }
