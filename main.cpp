184a23d64a0f405d9732269a54ee70b0ea48c05c
Tamer Maher <tmaher1982@gmail.com>
2021-04-24 14:05:21 +0200

    vectors

--- main.cpp
+++ main.cpp
@@ -207,12 +207,29 @@ int main()
 
     // return 0;
 
-    OrderBookEntry order1{1000, 
+    std::vector<OrderBookEntry> orders;
+
+    // OrderBookEntry order1{1000, 
+    //                       0.02, 
+    //                       "2020/03/17 17:01:24.884492", 
+    //                       "BTC/USDT", 
+    //                       OrderBookType::bid };
+
+    
+    orders.push_back(OrderBookEntry {1000, 
                           0.02, 
                           "2020/03/17 17:01:24.884492", 
                           "BTC/USDT", 
-                          OrderBookType::bid };
+                          OrderBookType::bid });
+    
+    orders.push_back(OrderBookEntry {2000, 
+                          0.02, 
+                          "2020/03/17 17:01:24.884492", 
+                          "BTC/USDT", 
+                          OrderBookType::bid });
+    
+    std::cout << "The price is " << orders[1].price << std::endl;
 
-    std::cout << "The price is " << order1.price << std::endl;
+    
 
 }
