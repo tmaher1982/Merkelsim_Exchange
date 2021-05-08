c68b0705623297242ca3ef00b8e2b84a3ffe91b4
Tamer Maher <tmaher1982@gmail.com>
2021-05-08 03:22:34 +0200

    added cide to tokenise function

--- CSVReader.cpp
+++ CSVReader.cpp
@@ -15,7 +15,24 @@ std::vector<OrderBookEntry> CSVReader::readCSV(std::string csvFile)
 
 std::vector<std::string> CSVReader::tokenise(std::string csvLine, char separator)
 {
+    
     std::vector<std::string> tokens;
+
+    signed int start, end;
+    std::string token;
+
+
+    start = csvLine.find_first_not_of(separator, 0); 
+    do {
+        end = csvLine.find_first_of(separator, start);
+        if (start == csvLine.length() || start == end) break;
+        if (end >=0) token = csvLine.substr(start, end - start);
+        else token = csvLine.substr(start, csvLine.length() - start);
+        tokens.push_back(token);
+        start = end + 1;
+    } while(end > 0);  
+
+
     return tokens;
 }
 
